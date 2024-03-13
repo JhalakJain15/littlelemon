ac454852df5300326292795485af628864f916c8
JhalakJain15 <jhalakjain590@gmail.com>
2024-03-13 21:00:44 +0530

    adding images

--- littlelemonproject/restaurant/views.py
+++ littlelemonproject/restaurant/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+# def sayHello(request):
+#     return HttpResponse('Hello World!')
+def index(request):
+    return render(request,'index.html',{})
 # Create your views here.
